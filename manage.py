17e3d619806f4796b204d7659fd5aa4d26d00d2c
je-evan <dhakaljeevan165@gmail.com>
2022-03-12 09:32:44 +0545

    change default port to 8025

--- manage.py
+++ manage.py
@@ -2,11 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management.commands.runserver import Command
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_intern_task.settings')
+    Command.default_port = 8025
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
